0d809bdd0454011998da58efc431885b2e147ba2
Aamir <aamirasaram2107@gmail.com>
2023-05-29 10:48:28 +0200

    Add add book functionality

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 const addBookBtn = document.querySelector('.add-book');
-const bookName = document.querySelector('.title');
-const bookAuthor = document.querySelector('.author');
+const bookName = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
+addBookBtn.addEventListener('click', addBook)
 
-let bookList = []
+let bookList = [];
 
 function addBook() {
-
     const bookObj = {
         title: bookName.value,
         author: bookAuthor.value,
     }
-
     bookList.push(bookObj);
-    
+
+    const books = document.querySelector('.books');
+    console.log(books);
+    const book = document.createElement('div');
+    book.classList.add('book');
+    book.innerHTML = `
+    <p>${bookObj.title}</p>
+    <p>${bookObj.author}</p>
+    `;
+
+    const removeBtn = document.createElement('button');
+    removeBtn.innerHTML = 'Remove';
+    removeBtn.addEventListener('click', () => {
+        //remove book from bookList
+    });
+    book.appendChild(removeBtn);
+    books.appendChild(book);
+
+    document.querySelector('.add-book-form').reset();
 }
-
-addBookBtn.addEventListener('click', addBook)
-
-
-
